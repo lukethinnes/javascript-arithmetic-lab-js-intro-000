15d3f6efe4721da3dd9a5e37902ea93072a06776
Luke Thinnes <lukethinnes@yahoo.com>
2020-09-11 03:43:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 function add(a, b) {
-  number = (a += b);
+  function number = (a += b);
   return('number');
 }
 function subtract(a, b) {
-  number = (a -= b);
+  function number = (a -= b);
   return('number');
-
 }
 function multiply(a, b) {
-  number = (a *= b);
+  function number = (a *= b);
   return ('number');
 }
 function divide(a, b) {
-  number = (a /= b);
+  function number = (a /= b);
   return ('number');
 }
 
